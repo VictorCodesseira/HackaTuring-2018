25b752d75c17c2f1eb8749ad0c9f2d734715a497
Victor Chacon <victor.codesseira@gmail.com>
2018-09-16 06:34:38 -0300

    Database prep

--- main.py
+++ main.py
@@ -11,66 +11,120 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+import datetime
 
-# Load dataset
-url = "hackaturing.dsv"
-dtype = {'base_hackaturing.cnpj': str,
-         'base_hackaturing.prestador': str,
-         'base_hackaturing.uf': str,
-         'base_hackaturing.id_beneficiario': str,
-         'base_hackaturing.sexo': str,
-         'base_hackaturing.data_nascimento': str,
-         'base_hackaturing.id_conta': str,
-         'base_hackaturing.crm_solicitante': str,
-         'base_hackaturing.cbos_solicitante': str,
-         'base_hackaturing.cbos_executante': str,
-         'base_hackaturing.data_entrada': str,
-         'base_hackaturing.data_saida': str,
-         'base_hackaturing.data_item': str,
-         'base_hackaturing.senha': str,
-         'base_hackaturing.tipo_guia': str,
-         'base_hackaturing.tipo_item': str,
-         'base_hackaturing.carater_atendimento': str,
-         'base_hackaturing.servico': str,
-         'base_hackaturing.quantidade': float,
-         'base_hackaturing.valor_item': float,
-         'base_hackaturing.valor_cobrado': float,
-         'base_hackaturing.valor_pago': float,
-         'base_hackaturing.ano_mes': str,
-         'base_hackaturing.cid': str}
-dataset = pandas.read_csv(url, sep = '|', dtype = dtype)
-names = {old: old[17:] for old in dtype.keys()}
-new_columns = [old_name[17:] for old_name in dataset.columns]
-dataset.columns = new_columns
+import orizonDB
+
+def get_history(analysisDB, id_):
+    sexo = analysisDB['sexo'][id_]
+    if sexo is not int:
+        sexo = sexo.values[0]
+    historico = analysisDB.loc[[id_], ['data_item', 'servico', 'carater_atendimento', 'tipo_guia']]
+    datas = [datetime.date.fromisoformat(value.split(' ')[0]) for value in historico['data_item']]
+    datas.sort()
+    if len(datas) == 1:
+        media = 0
+        mediana = 0
+    else:
+        delta_datas = []
+        for i in range(1, len(datas)):
+            delta_datas.append((datas[i] - datas[i-1]).days)
+
+        mediana = delta_datas[len(delta_datas)//2 ]
+        media = 0
+        for d in delta_datas:
+            media += d
+
+        media /= len(delta_datas)
+    data_nascimento = analysisDB['data_nascimento']
+    if data_nascimento is not datetime.datetime:
+        data_nascimento = data_nascimento.values[0]
+    idade = (datas[-1]- datetime.date.fromisoformat(data_nascimento)).days
+    servicos = historico['servico'].values
+    soma_servicos = [0]*len([value.strip('[,]') for value in servicos[0].split(' ')])
+    for servico in servicos:
+        servico = [value.strip('[,]') for value in servico.split(' ')]
+        for i in range(len(servico)):
+            soma_servicos[i] += int(servico[i])
+
+    guias = historico['tipo_guia'].values
+    soma_guias = [0]*len([value.strip('[]') for value in guias[0].split(',')])
+    for guia in guias:
+        guia = [value.strip('[]') for value in guia.split(',')]
+        for i in range(len(guia)):
+            soma_guias[i] += int(guia[i])
+
+    caraters = historico['carater_atendimento'].values
+    soma_carater = 0
+    for carater in caraters:
+        soma_carater += carater
+
+    data = [sexo]+[idade//365]+[media]+[mediana]+soma_guias+[soma_carater]+soma_servicos
+    return pandas.DataFrame(data)
+
+def main():
+    mainDB = orizonDB.get_DB()
+    relevant_columns = ['id_beneficiario',
+                        'carater_atendimento',
+                        'valor_item',
+                        'tipo_guia',
+                        'sexo',
+                        'data_nascimento',
+                        'data_item',
+                        'senha',
+                        'servico']
+    analysisDB = mainDB[relevant_columns][:20000]
+    size = analysisDB.shape[0]
+    analysisDB["carater_atendimento"].replace(["URGENCIA", "ELETIVO"], [1, 0], inplace = True)
+    analysisDB["sexo"].replace(["M", "F"], [0, 1], inplace = True)
+    analysisDB["tipo_guia"].replace(["Consulta", "Honorario", "Internacao", "SADT"],["[1,0,0,0]","[0,1,0,0]","[0,0,1,0]","[0,0,0,1]"], inplace = True)
+    analysisDB.set_index('id_beneficiario', inplace = True)
+    sorted_by_value = analysisDB.sort_values('valor_item', ascending = False)
+    classes = []
+    for i in range(size):
+        valor = sorted_by_value['valor_item'][i]
+        servico = sorted_by_value['servico'][i]
+        if valor > 1000 and servico not in classes:
+            classes.append(servico)
+    frequency = analysisDB.groupby("servico").count().sort_values('sexo', ascending = False)
+    limit = size/20
+    others = []
+    for i in range(frequency.shape[0]):
+        num = frequency["sexo"][i]
+        servico = frequency.index[i]
+        if servico not in classes:
+            if num > limit:
+                classes.append(servico)
+            else:
+                others.append(servico)
 
 
-print(dataset.columns)
-# print(dataset.shape)
-# print(dataset.head(20))
-# print(dataset.describe())
+    tamanho_hot_encoding = len(classes) + int(bool(others))
+    servico_hot_encoding = [str([1 if i == j else 0 for j in range(tamanho_hot_encoding)]) for i in range(tamanho_hot_encoding)]
+    analysisDB["servico"].replace(classes, servico_hot_encoding[:tamanho_hot_encoding - 1], inplace = True)
+    analysisDB["servico"].replace(others, servico_hot_encoding[-1], inplace = True)
 
-# dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+    lookout_histories = pandas.DataFrame(columns = ['id', 'history', 'output'])
+    for i in range(len(analysisDB.values)):
+        valor_servico = 0
+        line = analysisDB.values[i]
+        servico = [value.strip('[,]') for value in line[7].split(' ')]
+        for i in range(len(servico)):
+            v = servico[i]
+            if v == '1':
+                valor_servico = i
 
-# scatter plot matrix
-# scatter_matrix(dataset)
-# plt.show()
+        carater_atendimento = line[0]
+        valor_item = line[1]
+        tipo_guia = line[2]
+        if carater_atendimento == 0 or (str(valor_item).lower() != 'nan' and int(valor_item) > 1000) or tipo_guia == '[0, 0, 1, 0]':
+            id_beneficiario = analysisDB.index[i]
+            if lookout_histories.empty or id_beneficiario not in lookout_histories.values[0]:
+                history = get_history(analysisDB, id_beneficiario)
+                history[valor_servico] = 0
+                line = pandas.DataFrame([[id, history, valor_servico]], columns = ['id', 'history', 'output'])
+                lookout_histories.append(line)
 
+    lookout_histories.sort_values('id')
 
-# # split array
-# array = dataset.values
-# X = array[:,0:4]
-# Y = array[:,4]
-# validation_size = 0.20
-# seed = 7
-# X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
-
-# # Test options and evaluation metric
-# seed = 7
-# scoring = 'accuracy'
-
-# knn = KNeighborsClassifier()
-# knn.fit(X_train, Y_train)
-# predictions = knn.predict(X_validation)
-# print(accuracy_score(Y_validation, predictions))
-# print(confusion_matrix(Y_validation, predictions))
-# print(classification_report(Y_validation, predictions))
+main()
---
+++ orizonDB.py
@@ -0,0 +1,33 @@
+import pandas
+
+def get_DB():
+    url = "hackaturing.dsv"
+    dtype = {'base_hackaturing.cnpj': str,
+            'base_hackaturing.prestador': str,
+            'base_hackaturing.uf': str,
+            'base_hackaturing.id_beneficiario': str,
+            'base_hackaturing.sexo': str,
+            'base_hackaturing.data_nascimento': str,
+            'base_hackaturing.id_conta': str,
+            'base_hackaturing.crm_solicitante': str,
+            'base_hackaturing.cbos_solicitante': str,
+            'base_hackaturing.cbos_executante': str,
+            'base_hackaturing.data_entrada': str,
+            'base_hackaturing.data_saida': str,
+            'base_hackaturing.data_item': str,
+            'base_hackaturing.senha': str,
+            'base_hackaturing.tipo_guia': str,
+            'base_hackaturing.tipo_item': str,
+            'base_hackaturing.carater_atendimento': str,
+            'base_hackaturing.servico': str,
+            'base_hackaturing.quantidade': float,
+            'base_hackaturing.valor_item': float,
+            'base_hackaturing.valor_cobrado': float,
+            'base_hackaturing.valor_pago': float,
+            'base_hackaturing.ano_mes': str,
+            'base_hackaturing.cid': str}
+    dataset = pandas.read_csv(url, sep = '|', dtype = dtype)
+    new_columns = [old_name[17:] for old_name in dataset.columns]
+    dataset.columns = new_columns
+    return dataset
+
